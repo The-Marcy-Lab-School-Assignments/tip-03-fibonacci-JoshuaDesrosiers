1f15e7b45c60c74d27b5003490c742195c500561
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2025-10-03 10:12:37 -0400

    hellooo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+ fibo=[0,1]
+ for(let x=0;x<n;x++){
+    x<=1?'':fibo[x]=fibo[x-1]+fibo[x-2]
+ }
+ console.log(fibo)
 }
 
 printFibonacci(5);  // should print 0, 1, 1, 2, 3
